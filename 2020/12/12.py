27bd76fc1be047b490eab02cdc8f87998b785347
Justin Rupert Reyes <justinrupert.reyes@gmail.com>
2021-05-22 04:15:17 +0900

    Complete day 12 part 1

---
+++ 2020/12/12.py
@@ -0,0 +1,75 @@
+
+class Ship:
+    directions = {
+        'N': (0,1),
+        'S': (0,-1),
+        'E': (1,0),
+        'W': (-1,0)
+    }
+    def __init__(self, file) -> None:
+        self._load_file(file)
+        self.x = 0
+        self.y = 0
+        self.bearing = 'E'
+        self.curr = 0
+    
+    def _load_file(self, file):
+        with open(file) as f:
+            self.instructions = [line.strip() for line in f if not line.isspace()]
+
+    def step(self):
+        curr_inst = self.instructions[self.curr]
+        inst, val = curr_inst[0], int(curr_inst[1:])
+        
+        if inst in 'NSEW':
+            self.move(val, inst)
+        elif inst in 'LR':
+            self.turn(val * (1 if inst == 'R' else -1))
+        elif inst == 'F':
+            self.move(val)
+        self.curr += 1
+        return self
+
+    def execute(self):
+        while not self.is_end_of_trip():
+            self.step()
+
+    def is_end_of_trip(self):
+        return self.curr >= len(self.instructions)
+
+    def move(self, val, bearing = None):
+        if bearing is None:
+            bearing = self.bearing
+
+        dir_x, dir_y = Ship.directions[bearing]
+
+        self.x += dir_x * val
+        self.y += dir_y * val
+
+    def turn(self, angle):
+        angle %= 360
+        dir = angle // 90
+        directions = 'ESWN'
+        curr_dir = directions.find(self.bearing)
+        curr_dir = (curr_dir + dir) % 4
+        self.bearing = directions[curr_dir]
+
+    def get_manhattan_distance(self):
+        return abs(self.x) + abs(self.y)
+    
+    def __str__(self):
+        return f'x: {self.x} y: {self.y} bearing: {self.bearing} manhattan distance: {self.get_manhattan_distance()}'
+
+if __name__ == '__main__':
+    
+    test_ship = Ship('test')
+
+    while not test_ship.is_end_of_trip():
+        print(test_ship.step())
+
+    print()
+
+    ship = Ship('input')
+    print(ship)
+    while not ship.is_end_of_trip():
+        print(ship.step())
